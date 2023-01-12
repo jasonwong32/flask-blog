79e3ed70cbe7948d79696fdee34d8bfc020f821f
jasonwong32 <jasonwong32@outlook.com>
2023-01-12 21:43:35 +0800

    add new column for user model

--- api/user/model.py
+++ api/user/model.py
@@ -4,6 +4,9 @@ from api import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(32), nullable=True)
+    last_name = db.Column(db.String(32), nullable=True)
+    birthdate = db.Column(db.Date, nullable=True)
     password = db.Column(db.String(512), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=True)
     admin = db.Column(db.Boolean, nullable=False, default=False)
